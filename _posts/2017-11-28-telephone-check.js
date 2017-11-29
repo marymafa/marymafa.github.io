29e8c2bcd174ba9db583fdb15e0d5f6555dd5985
marymafa <marymafa80@gmail.com>
2017-11-29 08:30:42 +0000

    made changes

--- _posts/2017-11-28-telephone-check.js
+++ _posts/2017-11-28-telephone-check.js
@@ -11,7 +11,7 @@ date: 2017-11-28
 
 Method 1
 
-I want continue with this chalenge because i didn't finish it like it was returning all false insteadd of returning true if is valid and false if is invalid, So I went and look-up on what it is. I’m going to start by creating a variable where the regular expression will be stored, I’ll get to the actual function later. First, i want to  make sure that if we get a country code, it’s equal to 1. To achieve this, I’m going to use the ^ symbol, which matches the beginning of a string. Then, I type in 1, since that’s what i actually want at that position. Since the country code is optional, using the ? symbol after it will match  zero or one of the preceding element for example(varchecker = /^1?/;).
+
 
 method 2
 
@@ -25,3 +25,6 @@ It’s actually not that simple, I’ll need to match any number and also escape
 method 4
 
 To match any digit, I can use this token: \d. If i place three of this in succession (\d\d\d) it will match any 3 digits in a row. i can also use this notation: \d{3}. This means that the previous token will repeat 3 times. i want to use this knowledge and implement it into my regular expression.
+It may seem quite confusing, but (\d{3}|\(\d{3}\)) just means: three digits in a row or opening parenthesis, three digits and closing parenthesis.
+
+I only have the actual phone number left, but we need to account for the possibility of a space or dash (-) character between the area code and phone number first. We will use a range by using the square brackets ([ and ]) for this.
